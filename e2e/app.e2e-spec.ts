8a855983e3d94544bc1b5723ea4fcc7ae802ce0b
Carlos Fernando dos Santos <fer.1972@hotmail.com>
2018-02-05 17:42:53 -0200

    Atualiza projeto.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AdventechClientPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('adventech-client App', function() {
-  let page: AdventechClientPage;
+describe('adventech-client App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AdventechClientPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
