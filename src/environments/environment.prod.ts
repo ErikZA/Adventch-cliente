385fd60f65d18be1bdf65ba298cfec7f7aef0ff0
Ana Carolina Beloti <ana.beloti@forlogic.net>
2018-05-30 13:43:34 -0300

    add caminho prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://adventech-api.azurewebsites.net/api',
-  apiUrlReport: 'http://localhost:58000/api'
+  apiUrlReport: 'http://adventech-stimulsoft.azurewebsites.net/api'
 };
