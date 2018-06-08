005737a0748cdea5e2d738307c1ed8736149baf5
kassia catarine <kassia_catarine.15@hotmail.com>
2018-06-08 08:11:03 -0300

    (melhoria) Página 404

--- src/app/shared/page-not-found/page-not-found.component.ts
+++ src/app/shared/page-not-found/page-not-found.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { MatDialogRef, MatDialog } from '@angular/material';
+import { ChangePasswordComponent } from '../../core/components/password/change-password/change-password.component';
 
 @Component({
   selector: 'app-page-not-found',
-  templateUrl: './page-not-found.component.html'
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['../layout/layout.component.scss', 'page-not-found.component.scss']
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor() { }
+  get year(): number { return new Date().getFullYear(); }
+  dialogRef: MatDialogRef<ChangePasswordComponent>;
+
+  constructor(
+    public authService: AuthService,
+    private dialog: MatDialog,
+  ) { }
 
   ngOnInit() {
+  }
+
+  logoff() {
+    this.authService.logoff();
+  }
+
+  changePassword() {
+    this.dialogRef = this.dialog.open(ChangePasswordComponent, {
+      width: '400px',
+      height: '500px'
+    });
+    this.dialogRef.afterClosed().subscribe(result => {
+      if (!result) { return; }
+    });
   }
 
 }
