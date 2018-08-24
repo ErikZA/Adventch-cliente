391e745472604c5841efe0ede6db785b108679ff
William Simão <william@forlogic.net>
2018-08-24 17:28:13 -0300

    atualizando relatório de avaliações

--- src/app/modules/treasury/components/avaliation/avaliation-data/avaliation-data.component.ts
+++ src/app/modules/treasury/components/avaliation/avaliation-data/avaliation-data.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MatSidenav } from '@angular/material';
+import { MatSidenav, MatSnackBar } from '@angular/material';
 
 import { Subject, Subscription } from 'rxjs';
 import { Observable } from 'rxjs/Observable';
@@ -52,7 +52,8 @@ export class AvaliationDataComponent implements OnInit, OnDestroy {
     private router: Router,
     private route: ActivatedRoute,
     private store: AvaliationStore,
-    private reportService: ReportService
+    private reportService: ReportService,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
@@ -174,10 +175,29 @@ export class AvaliationDataComponent implements OnInit, OnDestroy {
     if (!avaliation) {
       return "Aguardando";
     }
+    return this.getLabel(avaliation.status);
+  }
+
+  public getStatusColor(churchAvaliation: ChurchAvaliation): any {
+    var avaliation = churchAvaliation.avaliations.filter(f => this.getMonth(f.date) === this.filterMonth && this.getYear(f.date) === this.filterYear && f.isMensal)[0];
+    if (!avaliation) {
+      return { "background-color": "#F93434" };
+    }
     switch (avaliation.status) {
       case EAvaliationStatus.Valued:
-        return "Avaliado";
+        return { "background-color": "#f1aa40" };
       case EAvaliationStatus.Finished:
+        return { "background-color": "#91c957" };
+      default:
+        return { "background-color": "#F93434" };
+    }
+  }
+
+  private getLabel(id): string {
+    switch (id) {
+      case 2:
+        return "Avaliando";
+      case 3:
         return "Finalizado";
       default:
         return "Aguardando";
@@ -202,25 +222,24 @@ export class AvaliationDataComponent implements OnInit, OnDestroy {
         element.download = 'avaliacoes-relatorio_geral.pdf';
         element.target = '_blank';
         element.click();
-        //this.snackBar.open('Gerando relatório!', 'OK', { duration: 5000 });
+        this.snackBar.open('Gerando relatório!', 'OK', { duration: 5000 });
     }, err => {
       console.log(err);
-        //this.snackBar.open('Erro ao gerar relatório relatório!', 'OK', { duration: 5000 });
+        this.snackBar.open('Erro ao gerar relatório relatório!', 'OK', { duration: 5000 });
     });
   }
 
   private getDataParams(): any {
     const district = this.districts.find(f => f.id === this.filterDistrict);
     const analyst = this.analysts.find(f => f.id === this.filterAnalyst);
-    //const period = this.filterPeriod;
     return {
       statusId: this.filterStatus,
-      //statusName: this.getStatusString(this.filterStatus),
+      statusName: this.getLabel(this.filterStatus),
       districtId: this.filterDistrict,
       districtName: district === undefined ? 'TODOS' : district.name,
       analystId: this.filterAnalyst,
       analystName: analyst === undefined ? 'TODOS' : analyst.name,
-      period: this.filterYear,
+      year: this.filterYear,
     };
   }
 
