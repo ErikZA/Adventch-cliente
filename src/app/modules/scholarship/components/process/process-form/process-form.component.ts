15a42f0a5c507ab738d3e67b77d2f01f1716840e
Alexandre Magno Monteiro <alexandre.magno@forlogic.net>
2018-08-30 15:08:32 -0300

    (fix) novo processo

--- src/app/modules/scholarship/components/process/process-form/process-form.component.ts
+++ src/app/modules/scholarship/components/process/process-form/process-form.component.ts
@@ -90,9 +90,12 @@ export class ProcessFormComponent implements OnInit, OnDestroy {
           this.processId = parsed;
           this.process = process;
         });
+      } else {
+        if (auth.getCurrentUser().idSchool === 0 && this.scholarshipService.schoolSelected <= 0) {
+          this.closeSidenav();
+        }
       }
     });
-
     this.processDataComponent.sidenavRight.open();
   }
   ngOnDestroy() {
@@ -124,13 +127,12 @@ export class ProcessFormComponent implements OnInit, OnDestroy {
     responsibleCpfForm.valueChanges.subscribe((cpf: string) => {
       if (responsibleCpfForm.valid) {
         this.scholarshipService.getResponsible(cpf).subscribe(responsible => {
-          if (responsible) {
-            this.students = Array.isArray(responsible.students) ? responsible.students : [];
-            this.setFormValuesResponsible(responsible);
-            this.responsible = responsible;
-          } else {
-            this.resetInformationsResponsible();
+          if (!responsible) {
+            return;
           }
+          this.students = Array.isArray(responsible.students) ? responsible.students : [];
+          this.setFormValuesResponsible(responsible);
+          this.responsible = responsible;
         });
       } else {
         this.resetInformationsResponsible();
@@ -230,7 +232,7 @@ export class ProcessFormComponent implements OnInit, OnDestroy {
       responsible: {
         cpf: this.formProcess.value.cpf,
         email: this.formProcess.value.email,
-        id: Number.isInteger(this.responsible.id) ? this.responsible.id : null,
+        id: !!this.responsible ? Number.isInteger(this.responsible.id) ? this.responsible.id : 0 : 0,
         name: this.formProcess.value.name,
         phone: this.formProcess.value.phone
       },
@@ -240,7 +242,7 @@ export class ProcessFormComponent implements OnInit, OnDestroy {
       serieId: this.formProcess.value.studentSerieId,
       documents: this.getAllDocumentsFromTypes(),
       student: {
-        id: Number.isInteger(this.selectStudent.id) ? this.selectStudent.id : null,
+        id: !!this.selectStudent ? Number.isInteger(this.selectStudent.id) ? this.selectStudent.id : 0 : 0,
         rc: this.formProcess.value.rc,
         name: this.formProcess.value.nameStudent,
       }
