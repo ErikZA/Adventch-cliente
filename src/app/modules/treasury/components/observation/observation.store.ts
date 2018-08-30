0abf0bddb167a721d9b886eb6de07fee50bb8fa1
William Simão <william@forlogic.net>
2018-08-30 09:40:42 -0300

    (fix) corrigindo o load dos filtros de observação

--- src/app/modules/treasury/components/observation/observation.store.ts
+++ src/app/modules/treasury/components/observation/observation.store.ts
@@ -121,6 +121,7 @@ export class ObservationStore {
     this.loadResponsibles();
   }
   private loadChurches() {
+    this.churches = new Array<Church>();
     if (this.dataStore.observations != null) {
       this.dataStore.observations.forEach(observation => {
         if (this.churches.map(x => x.id).indexOf(observation.church.id) === -1) {
@@ -142,6 +143,7 @@ export class ObservationStore {
   }
 
   private loadResponsibles() {
+    this.responsibles = new Array<User>();
     this.dataStore.observations.forEach(observation => {
       if (this.responsibles.map(x => x.id).indexOf(observation.responsible.id) === -1) {
         this.responsibles.push(observation.responsible);
