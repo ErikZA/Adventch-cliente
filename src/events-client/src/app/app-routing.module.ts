475b672a5693a16932a55ba7b298503082c594cc
Guilherme Vinicius <guilherme@adven.tech>
2020-01-31 14:35:22 -0300

    adicionando authRouter-guard

--- src/events-client/src/app/app-routing.module.ts
+++ src/events-client/src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared/auth/auth.guard';
 import { LoginComponent } from './modules/account/login/login.component';
 import { ForgotComponent } from './modules/account/forgot/forgot.component';
+import { AuthRouteService } from './shared/auth/authRoute.guard';
 
 const routes: Routes = [
   { path: 'inscricoes', loadChildren: '../app/modules/events/event-register.module#EventRegisterModule', canActivate: [AuthGuard] },
   { path: 'eventos', loadChildren: '../app/modules/events/events.module#EventsModule', canActivate: [AuthGuard] },
   { path: 'configuracoes', loadChildren: '../app/modules/settings/settings.module#SettingsModule', canActivate: [AuthGuard] },
 
-  { path: '', component: LoginComponent },
-  { path: 'recuperar-senha', component: ForgotComponent },
+  { path: '', component: LoginComponent, canActivate: [AuthRouteService] },
+  { path: 'recuperar-senha', component: ForgotComponent, canActivate: [AuthRouteService] },
 ];
 
 @NgModule({
