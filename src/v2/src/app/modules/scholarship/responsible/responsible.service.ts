a2f52d92f32f2a1ab98934bef3ee8f5d70bf0980
Erik Zambeli <erik@adven.tec>
2020-02-03 16:42:16 -0300

    Setando ambiente de Homologacao

--- src/v2/src/app/modules/scholarship/responsible/responsible.service.ts
+++ src/v2/src/app/modules/scholarship/responsible/responsible.service.ts
@@ -6,7 +6,7 @@ import { HttpEventType } from '@angular/common/http';
 
 import { Observable, Subject } from 'rxjs';
 
-import { environment } from '../../../../environments/environment.prod';
+import { environment } from '../../../../environments/environment.homolog';
 import { ResponsibleUploadInterface } from '../interfaces/responsible-upload-interface';
 @Injectable()
 export class ResponsibleService {
