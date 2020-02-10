2052ba25d1cef19862714405d8ab95ffbcd7c5b1
Erik Henrique de Oliveira Zambeli <erik@adven.tech>
2020-02-10 18:19:35 +0000

    Added environment.ts

---
+++ src/v2/src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { EnvironmentInterface } from './environment-interface';
+
+export const environment: EnvironmentInterface = {
+  production: true,
+  apiUrl: 'https://adventech-api.azurewebsites.net/api',
+  apiUrlReport: 'https://adventech-stimulsoft.azurewebsites.net/api'
+};
