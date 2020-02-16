082c14d37b9f144ffd3cb3f66ea64cb4e1b6afe5
Guilherme Vinicius <guilherme@adven.tech>
2020-02-16 18:01:58 -0300

    Configurando o environments

--- src/identityAdventech/src/environments/environment.ts
+++ src/identityAdventech/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  name: 'dev',
+  project: 'identity',
+  client_id: '307316f6-be2e-43c5-92c4-688dfb797c1a',
+  identityApiUrl: 'http://localhost:5000/api/identity/v1',
 };
 
 /*
