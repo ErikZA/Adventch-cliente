77e7b09feb748dbe96ec434f0a1721d5185e10ae
João Wicktor <joao.ortiz@adven.tech>
2020-02-28 20:28:29 +0000

    Updated environment.prod.ts

--- src/identityAdventech/src/environments/environment.prod.ts
+++ src/identityAdventech/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  name: 'prod',
+  project: 'identity',
+  client_id: '307316f6-be2e-43c5-92c4-688dfb797c1a',
+  redirect_url: 'https://login.adven.tech',
+  identityApiUrl: 'https://api-identity.adven.tech/api/identity/v1',
 };
