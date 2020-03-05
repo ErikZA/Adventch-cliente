68d66e22fdc5b5d82da48f9fd406d4f7f15cb342
Guilherme Vinicius <guilherme@adven.tech>
2020-03-05 16:56:10 -0300

    env

--- src/identityAdventech/src/environments/environment.ts
+++ src/identityAdventech/src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
   name: 'dev',
   project: 'identity',
   client_id: '5061286c-4541-44f0-a4b7-0fe147b4e8a1',
-  redirect_url: 'https://login.adven.tech',
+  redirect_url: 'https://login.adven.tech/#/',
   identityApiUrl: 'https://api-identity.adven.tech/api/identity/v1',
 };
 
